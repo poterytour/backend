0db562f20dc1d06284f444809f9fae0d9461dafb
palpitate-wx <2281342596@qq.com>
2022-10-24 22:27:57 +0800

    初始化项目

---
+++ src/main/java/com/example/poetrytour/PoetrytourApplication.java
@@ -0,0 +1,13 @@
+package com.example.poetrytour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoetrytourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoetrytourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/poetrytour/PoetrytourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.poetrytour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoetrytourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
